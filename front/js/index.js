edb8f16c1b73d11a00261ab86e5f1c7d39ce1166
Maxim Bobrovskii <m.bobrovskij2020@gmail.com>
2022-11-23 13:50:09 +0300

    work fetch and no global changes

--- front/js/index.js
+++ front/js/index.js
@@ -1,17 +1,57 @@
 const AUTHOR = "author";
 const QUOTE = "quote";
+const EMPTY_QUOTE = "Либо у преподавателя нет цитат, либо вы ввели не правильную фамилию"
 
-const db = JSON.parse(data);
+let db = JSON.parse(data);
 const authors = [];
 const select = document.querySelector(`#${AUTHOR}`);
+const select_search = document.querySelector('#author_search')
 
 document.querySelector("#submitButon").addEventListener("click", updateQuote);
+document.querySelector("#submitButon_search").addEventListener("click", updateQuoteSelect);
+
+function authorCheck(authors, author) {
+    let c = false
+    authors.forEach((element) => {
+        let a = element.split(" ")[0]
+        let b = author.split(" ")[0]
+        if (a === b) {
+            c = true
+        }
+    });
+    return c;
+}
+
+db = db.sort(function (a, b) {
+   if (a.author < b.author) {
+       return -1
+   }  else {
+       return 1
+   }
+});
+
+async function getTextAnswer(url) {
+    return await fetch(url).then(result => {
+        return result.text()
+    }).then(responseResult => {
+        return responseResult
+    })
+}
+
+
+getTextAnswer("https://functions.yandexcloud.net/d4eahqh17rjle04km5ot").then(elem => {
+    let db = JSON.parse(elem.toString());
+    console.log(db)
+})
+
 
 for (let note of db) {
-    authors.push(note.author);
-    let option = document.createElement("option");
-    option.innerText = note.author;
-    select.append(option);
+    if (!authorCheck(authors, note.author) && note.author.length < 30) {
+        authors.push(note.author);
+        let option = document.createElement("option");
+        option.innerText = note.author;
+        select.append(option);
+    }
 }
 
 function getRandomInt(min, max) {
@@ -21,12 +61,11 @@ function getRandomInt(min, max) {
 }
 
 function updateQuote(event) {
-    const currentAuthor = select.value;
+    let currentAuthor = select.value;
 
     const allQuotesCurrentAuthor = [];
-
     for (let note of db) {
-        if (note.author == currentAuthor) {
+        if (note.author === currentAuthor) {
             allQuotesCurrentAuthor.push(note.quote);
         }
     }
@@ -35,4 +74,23 @@ function updateQuote(event) {
 
     document.querySelector(`#${QUOTE}`).innerHTML =
         allQuotesCurrentAuthor[randValue];
+}
+
+function updateQuoteSelect(event) {
+    const currentAuthor = select_search.value;
+
+    const allQuotesCurrentAuthor = [];
+    for (let note of db) {
+        if (note.author.split(" ")[0] === currentAuthor) {
+            allQuotesCurrentAuthor.push(note.quote);
+        }
+    }
+    if(allQuotesCurrentAuthor.length === 0) {
+        document.querySelector(`#${QUOTE}`).innerHTML = EMPTY_QUOTE
+    } else {
+        const randValue = getRandomInt(0, allQuotesCurrentAuthor.length);
+
+        document.querySelector(`#${QUOTE}`).innerHTML =
+            allQuotesCurrentAuthor[randValue];
+    }
 }
